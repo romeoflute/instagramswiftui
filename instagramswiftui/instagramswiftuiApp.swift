56cc1687bab92a3001d832763d914b680a4b3fca
meoflauta <meoflauta@gmail.com>
2020-12-04 17:52:42 +0800

    Initial Commit

---
+++ instagramswiftui/instagramswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramswiftuiApp.swift
+//  instagramswiftui
+//
+//  Created by Romeo Flauta on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct instagramswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
