63a4ffe8da580d2bd3b6b3cfb864faba3fab0bff
meoflauta <meoflauta@gmail.com>
2020-12-29 20:29:21 +0800

    Update gitignore

--- instagramswiftui/Helpers/Constants.swift
+++ instagramswiftui/Helpers/Constants.swift
@@ -19,7 +19,7 @@ let TEXT_EMAIL = "Email"
 let TEXT_USERNAME = "Username"
 let TEXT_PASSWORD = "Password"
 let TEXT_SIGNIN_HEADLINE = "Photo Sharing"
-let TEXT_SIGNIN_SUBHEADLINE = "The essiest way to share photos with family and friends"
+let TEXT_SIGNIN_SUBHEADLINE = "The easiest way to share photos with family and friends"
 let TEXT_SIGNUP_NOTE = "An account will allow you to save and access photo information across devices. You can delete your account at any time and your information will not be shared."
 let TEXT_SIGNUP_PASSWORD_REQUIRED = "At least 8 characters required"
 
